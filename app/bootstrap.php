c9fe0c7265341dd3cfe83fc769dc97be6991b355
nklrpn <mail@nklrpn.ru>
2018-04-19 22:29:04 +1000

    Modified bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,25 +10,4 @@ $twig = new Twig_Environment($twig_loader, [
     /*'cache' => __DIR__ . '/../cache'*/
 ]);
 
-/*$template = $twig->load('index.twig');
-echo $template->render([
-    'todos' => [
-        [
-            'id' => 1,
-            'text' => 'Taste JavaScript',
-            'is_completed' => true,
-        ],
-        [
-            'id' => 2,
-            'text' => 'Buy a unicorn',
-            'is_completed' => false,
-        ],
-        [
-            'id' => 3,
-            'text' => 'Try Twig',
-            'is_completed' => false,
-        ],
-    ],
-]);*/
-
 $controller = new App\Controller\Controller($twig);
