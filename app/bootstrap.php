a3bb316ceffca253dc3cc376e169589e0d89f8ec
nklrpn <mail@nklrpn.ru>
2018-04-23 21:43:02 +1000

    Modified bootstrap.php: added Doctrine settings

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,30 +1,45 @@
 <?php
-
 /**
  * Configuration
  */
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
 $dotenv->load();
 
-/** 
+/**
  * Twig
  */
-$twig_loader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
-$twig = new Twig_Environment($twig_loader, [
+$twigLoader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
+$twig = new Twig_Environment($twigLoader, [
     /*'cache' => __DIR__ . '/../cache'*/
 ]);
 
 /**
+ * Doctrine
+ */
+$dbParams = [
+    'driver' => getenv('APP_DATABASE_DRIVER'),
+    'host' => getenv('APP_DATABASE_HOST'),
+    'dbname' => getenv('APP_DATABASE_DBNAME'),
+    'user' => getenv('APP_DATABASE_USER'),
+    'password' => getenv('APP_DATABASE_PASSWORD'),
+];
+$isDevMode = true;
+
+$dbConf = Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration([__DIR__ . '/../src/Entity'], $isDevMode, null, null, false);
+$entityManager = Doctrine\ORM\EntityManager::create($dbParams, $dbConf);
+
+/**
  * Storage
  */
-$storage = new App\Storage\JsonStorage();
+//$storage = new App\Storage\JsonStorage();
+$storage = new App\Storage\DatabaseStorage($entityManager);
 
-/** 
+/**
  * Controller
  */
 $controller = new App\Controller\StorageController($twig, $storage);
 
-/** 
+/**
  * Router
  */
 $router = new App\Router();
