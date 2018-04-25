694549f1ff198d379a6bedf32e546e5cc80b41f1
nklrpn <mail@nklrpn.ru>
2018-04-25 21:00:44 +1000

    Modified index.php

--- public/index.php
+++ public/index.php
@@ -3,9 +3,3 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 require __DIR__ . '/../app/bootstrap.php';
 require __DIR__ . '/../app/routes.php';
-
-//var_dump($auth->isUsernameStored('user1'));
-//var_dump($auth->register('user1', 'password'));
-//var_dump($auth->login('user1', 'password'));
-//var_dump($auth->logout('user1'));
-//var_dump($auth->isLogged());
